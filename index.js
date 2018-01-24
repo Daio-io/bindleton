8d2cd7e658e72014ca95548a40013d4808f68924
Daio-io <dai.williams@bbc.co.uk>
2018-01-24 08:17:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const config = Object.freeze({
+    PORT: process.env.PORT || 3000,
+    CACHE: process.env.CACHE || 0
+  });
+
+const http = require('http');
+const Express = require('express');
+const app = Express();
+const Browser = require('zombie');
+
+
+const cheerio = require('cheerio')
+
+app.get('/status', (_, res) => {
+    res.send('OK')
+})
+
+app.get('/bins', (_, res) => {
+    const b = new Browser();
+    b.visit("https://www.salford.gov.uk/bins-and-recycling/bin-collection-days/your-bin-collections/?UPRN=10091475115", { runScripts: true }, () => {
+        let $ = cheerio.load(b.html())
+
+        let firstDate = $('.wastedate').first().text()
+        let secondDate = $('.wastedate').eq(1).text()
+        let thirdDate = $('.wastedate').eq(2).text()
+
+        let first = $('.wastecollection').first().text();
+        let next = $('.wastecollection').eq(1).text();
+        let third = $('.wastecollection').eq(2).text();
+
+        let firstImages = $('.centre').first()
+        let secondImages = $('.centre').eq(1)
+        let thirdImages = $('.centre').eq(2)
+
+        let images1 = []
+        $(firstImages).find('img').each((index, item) => {
+            let image = "https://www.salford.gov.uk" + $(item).attr('src')
+            images1.push(image)
+        })
+
+        let images2 = []        
+        $(secondImages).find('img').each((index, item) => {
+            let image = "https://www.salford.gov.uk" + $(item).attr('src')
+            images2.push(image)
+        })
+
+        let images3 = []
+        $(thirdImages).find('img').each((index, item) => {
+            let image = "https://www.salford.gov.uk" + $(item).attr('src')
+            images3.push(image)
+        })
+
+        let result = {
+            results: [
+                {
+                  text: first + " " + firstDate,
+                  date: firstDate,
+                  images: images1  
+                },
+                {
+                  text: next + " " + secondDate,
+                  date: secondDate,
+                  images: images2  
+                },
+                {
+                  text: third + " " + thirdDate,
+                  date: thirdDate,
+                  images: images3 
+                }
+            ]
+        }
+
+        res.json(result);
+
+        b.destroy();
+    })
+
+})
+
+const server = http.createServer(app);
+
+server.listen(config.PORT, () => {
+    console.log('Bins started on:', config.PORT);
+});
